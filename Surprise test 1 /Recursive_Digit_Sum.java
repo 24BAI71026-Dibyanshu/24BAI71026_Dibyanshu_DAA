6889871a41b1333a4c50f27b2c3d5e36c69cb60f
24BAI71026-Dibyanshu <krish9805438835@gmail.com>
2026-02-19 14:40:50 +0530

    Create Recursive_Digit_Sum.java

---
+++ Surprise test 1 /Recursive_Digit_Sum.java
@@ -0,0 +1,24 @@
+ public static int superDigit(String n, int k) {
+     
+       
+            long sum = 0;
+
+           for (int i = 0; i < n.length(); i++) {
+               sum += n.charAt(i) - '0';
+         }
+            sum *= k;
+            while (sum >= 10) {
+            long temp = 0;
+            while (sum > 0) {
+                temp += sum % 10;
+                sum /= 10;
+            }
+                sum = temp;
+            }
+
+            return (int) sum;
+   
+            
+        }
+
+    }
